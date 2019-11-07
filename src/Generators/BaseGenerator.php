31031a2fce5e8b93a53d2c1edddf7f3647b7ecc3
Igor Moraes <igor.sgm@gmail.com>
2019-11-07 00:39:08 -0800

    Base generator phpdoc blocks

--- src/Generators/BaseGenerator.php
+++ src/Generators/BaseGenerator.php
@@ -6,6 +6,13 @@ use Thomisticus\Generator\Utils\FileUtil;
 
 class BaseGenerator
 {
+    /**
+     * Rollback a file creation
+     *
+     * @param string $path
+     * @param string $fileName
+     * @return bool
+     */
     public function rollbackFile($path, $fileName)
     {
         if (file_exists($path . $fileName)) {
